3ff098bea525b20c4499b63c633f36e23df6981a
IAmFiveHigh <745170149@qq.com>
2017-09-14 17:17:19 +0800

    添加画布

---
+++ canvas/js/canvasJS.js
@@ -0,0 +1,13 @@
+/**
+ * Created by meitiannongzi on 2017/9/14.
+ */
+
+window.onload = function() {
+
+    var canvas = document.getElementById("canvas1")
+    var context = canvas.getContext("2d")
+
+    canvas.width = 1024
+    canvas.height = 768
+}
+
