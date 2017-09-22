884ebad15d8beb01308086a2dfbb97c960170656
IAmFiveHigh <745170149@qq.com>
2017-09-22 18:27:04 +0800

    添加排行榜

--- q&a/js/rankingJS.js
+++ q&a/js/rankingJS.js
@@ -1,3 +1,9 @@
 /**
  * Created by meitiannongzi on 2017/9/22.
  */
+window.onload = function () {
+
+    var ranks = document.getElementsByClassName("rank")
+    var myRank = ranks[2]
+    myRank.style.backgroundColor = "#fcf9e4"
+}
