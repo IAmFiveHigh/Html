b55bc21ad6003dc597300a9128efdc5713fceb73
IAmFiveHigh <745170149@qq.com>
2017-10-13 18:47:58 +0800

    添加WKWebView的链接吊起SFSafariController的代码和更新了答题

---
+++ WKWebView转SFSafariController/WKWebView转SFSafariController.js
@@ -0,0 +1,18 @@
+/**
+ * Created by meitiannongzi on 2017/10/13.
+ */
+
+
+var alinks = document.getElementsByTagName("a")
+for (var j=0; j<alinks.length; j++) {
+    var alink = alinks[j]
+
+
+    alink.onclick = function () {
+
+        window.webkit.messageHandlers.sfsafari.postMessage(this.href)
+
+        return false
+    }
+
+}
--- q&a/js/scoreJS.js
+++ q&a/js/scoreJS.js
@@ -10,8 +10,7 @@ window.onload = function () {
 
     var photoBackground = document.getElementById("photoBackground")
 
-    var scoreNum = parseInt(localStorage.getItem("score"))
-
+    var scoreNum = 10
     var photo = document.getElementById("photo")
     var photo_img = photo.getElementsByTagName("img")[0]
 
@@ -24,34 +23,59 @@ window.onload = function () {
         maxWidth = clientWidth
     }
 
-    document.getElementById("title").textContent = "你在答题中获得了" + scoreNum + "分"
+    document.getElementById("title").textContent = "获得" + scoreNum + "分"
 
     var sub_title = document.getElementById("subTitle")
     sub_title.style.width = maxWidth - 20 * 2 + "px"
     sub_title.style.left = maxleft + 20 + "px"
-    sub_title.style.top = 86 + "px"
+    sub_title.style.top = 64 + "px"
 
+
+    var sub_title_span = sub_title.getElementsByTagName("span")[0]
+
+    var comment = document.getElementById("comment")
+
+    var percent = Math.floor(Math.random() * 10)
+    var percent2 = Math.floor(Math.random() * 10)
     if (scoreNum === 100) {
-        photoBackground.src = "images/超神农业技术100.png"
-        sub_title.textContent = "通过测试，我对农药已经无所谓不知，属于超神农药技术员孤独求败段位——农药知识就是牛！"
+        photoBackground.src = "images/超神.png"
+        comment.textContent = "战斗实力可怕，什么病虫害都难不倒他！专业、靠谱、牛气冲天——大神，等着大家来膜拜吧"
+        sub_title_span.textContent = scoreNum + ".0%"
+
     }else if (scoreNum < 100 && scoreNum >= 80) {
-        photoBackground.src = "images/高级农业技术员80-90.png"
-        sub_title.textContent = "通过测试，我对杀菌剂、杀虫剂非常熟悉，对除草剂得心应手，已经荣升高级农药技术员段位！"
+        photoBackground.src = "images/高级.png"
+        comment.textContent = "在本卷中，只错了" + (scoreNum >= 90 ? 1 : 2) + "道题，病虫害识别又快又好，达到了高级病虫害技术阶段，技术杠杠的！一般人都比不过你了，不服来比~"
+        sub_title_span.textContent = (scoreNum + percent2) + "." + percent + "%"
+
     }else if (scoreNum < 80 && scoreNum >= 50) {
-        photoBackground.src = "images/中级农业技术员50-70.png"
-        sub_title.textContent = "我对农药还算了解，具备一个优秀农资经销商的潜力，加把劲你会更加优秀！！！"
-    }else if (scoreNum < 50 && scoreNum >= 10) {
-        photoBackground.src = "images/农业入门新手20-40.png"
-        sub_title.textContent = "分数这么少，是题库出问题了吗？不好意思给朋友看~~~~抓紧时间重新做一遍⋯⋯"
-    }else if (scoreNum == 0) {
-        photoBackground.src = "images/农资门外汉0-10.png"
-        sub_title.textContent = "农极客的系统崩溃了吗？我为什么只有零分？零分？"
+        photoBackground.src = "images/中级.png"
+        comment.textContent = "及格了！对一些常见病虫害有所了解，但还要更努力才能称得上是一个优秀农资人哦~"
+        sub_title_span.textContent = (scoreNum + percent2) + "." + percent + "%"
+
+    }else if (scoreNum < 50 && scoreNum > 10) {
+        photoBackground.src = "images/新手.png"
+        comment.textContent = "赶紧的，病虫害技术升级之路还任重而道远，抓紧时间再重新刷一遍题！！！"
+        sub_title_span.textContent = (scoreNum + percent2) + "." + percent + "%"
+
+    }else {
+        photoBackground.src = "images/门外汉.png"
+        comment.textContent = "肯定是故意得分这么少的，对不对？不敢相信……蒙都不至于蒙这点分啊——"
+        sub_title_span.textContent = (scoreNum + percent2) + "." + percent + "%"
+        sub_title.innerHTML = "<div style='text-align: center; font-size: 16px; color: #333333'>刚才，" + "XX" +"做题时，有<span>" + 9 + percent + "." + percent2 + "%</span>" + "人的分数超过了他"
+
     }
 
+    photoBackground.style.height = clientHeight * 0.425 + "px"
 
-    photoBackground.style.width = maxWidth * 0.85 + "px"
-    photoBackground.style.left = maxleft + maxWidth * 0.0725 + "px"
-    photoBackground.style.height = maxWidth * 0.85 * 0.773 + "px"
+    if (clientHeight * 0.425 / 0.7 > maxWidth) {
+        photoBackground.style.width = maxWidth + "px"
+        photoBackground.style.left = maxleft + "px"
+
+    } else {
+        photoBackground.style.width = clientHeight * 0.425 / 0.7 + "px"
+        photoBackground.style.left = maxleft + (maxWidth - clientHeight * 0.425 / 0.7) / 2 + "px"
+
+    }
 
     var sub_title_bottom = sub_title.offsetTop + sub_title.offsetHeight - 20
     photoBackground.style.top = sub_title_bottom + "px"
@@ -61,9 +85,15 @@ window.onload = function () {
     photo_img.style.height = maxWidth / 4.3 + "px"
 
     var centerY = (maxWidth * 0.85 * 0.773) * 0.56 +  sub_title_bottom
-    console.log(photoBackground.offsetHeight)
     photo.style.top = centerY - maxWidth / 4.3 / 2 + "px"
 
+    comment.style.top = photoBackground.offsetTop + photoBackground.offsetHeight + 20 + "px"
+    comment.style.left = maxleft + 12 + "px"
+    comment.style.width = clientWidth - 12 * 2 + "px"
+
+    var tmpH = comment.offsetHeight
+
+    var tmpT = photoBackground.offsetTop + photoBackground.offsetHeight + 20 + tmpH
 
     //二维码
     var qr_code = document.getElementById("QR_code")
@@ -71,7 +101,7 @@ window.onload = function () {
     qr_code_img.style.width = maxWidth / 3.5 + "px"
     qr_code_img.style.height = maxWidth / 3.5 + "px"
 
-    qr_code.style.top = photoBackground.offsetTop + photoBackground.offsetHeight + 30 + "px"
+    qr_code.style.top = tmpT + 20 + "px"
     qr_code.style.left = maxWidth / 8.1 + maxleft + "px"
 
 
@@ -81,22 +111,22 @@ window.onload = function () {
     finger_print_img.style.width = maxWidth / 10 + "px"
     finger_print_img.style.height = maxWidth / 10 + "px"
 
-    finger_print.style.top = photoBackground.offsetTop + photoBackground.offsetHeight + 40 + "px"
+    finger_print.style.top = tmpT + 30 + "px"
     finger_print.style.left = maxWidth / 8.1 + maxWidth / 3.5 + maxWidth / 6.25 + maxleft + "px"
 
     //提示语
     var span1 = document.getElementById("span1")
     span1.style.left = finger_print.style.left
-    span1.style.top = photoBackground.offsetTop + photoBackground.offsetHeight + 40 + maxWidth / 10 + 15 + "px"
+    span1.style.top = tmpT + 30 + maxWidth / 10 + 15 + "px"
 
     var span2 = document.getElementById("span2")
     span2.style.left = finger_print.style.left
-    span2.style.top = photoBackground.offsetTop + photoBackground.offsetHeight + 40 + maxWidth / 10 + 15 + 19 + "px"
+    span2.style.top = tmpT + 30 + maxWidth / 10 + 15 + 19 + "px"
 
     //解析按钮
     var analysis = document.getElementById("analysis_btn")
     analysis.style.left = qr_code.style.left
-    analysis.style.top = photoBackground.offsetTop + photoBackground.offsetHeight + maxWidth / 3.5 + 30 + 30 + "px"
+    analysis.style.top = tmpT + maxWidth / 3.5 + 30 + 20 + "px"
 
     analysis.onclick = function () {
         window.location.href = "result.html"
